7bcdced4d29d53fe25b2e04c99c8cc60fe8c3bf5
Ms_Natali <golubkova@greensight.ru>
2024-05-22 18:01:04 +0300

    ENSITECH-74

--- config/serve-swagger.php
+++ config/serve-swagger.php
@@ -5,7 +5,7 @@ return [
 
     'urls' => [
 //        [
-//            'url' => '/api-docs/v1/index.yaml', // Path in public
+//            'url' => '/api-docs/v1/index.yaml', // Path in public dir
 //            'name' => 'API v1' // The name to display in the interface
 //        ],
     ],
